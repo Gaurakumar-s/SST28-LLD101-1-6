4a1f32b2afa6547219fe1e15d8667954f98ead39
gauravkumar <Student-sst@outlook.com>
2026-02-23 00:26:36 +0530

    Ex1 Solved, comments to be added

---
+++ SOLID/Ex1/src/ParsedData.java
@@ -0,0 +1,13 @@
+public class ParsedData {
+    public final String name;
+    public final String email;
+    public final String phone;
+    public final String program;
+
+    public ParsedData(String name, String email, String phone, String program) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.program = program;
+    }
+}
