ba1befde65aa40dcac09fe52154e7395458a0e2e
gauravkumar <Student-sst@outlook.com>
2026-02-23 10:46:25 +0530

    Ex5 Solved, comments to be added

--- SOLID/Ex5/src/Exporter.java
+++ SOLID/Ex5/src/Exporter.java
@@ -1,4 +1,11 @@
 public abstract class Exporter {
     // implied "contract" but not enforced (smell)
-    public abstract ExportResult export(ExportRequest req);
+    public ExportResult export(ExportRequest req) {
+        if (req == null) {
+            return ExportResult.error("request is null");
+        }
+        return doExport(req);
+    }
+    
+    protected abstract ExportResult doExport(ExportRequest req);
 }
